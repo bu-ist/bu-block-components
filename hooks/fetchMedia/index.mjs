ab122d8ef987bf6c3412150b7096f1efc7c7501c
Tim King <timking@bu.edu>
2025-06-26 16:34:12 -0400

    it doesn’t work anymore

--- hooks/fetchMedia/index.mjs
+++ hooks/fetchMedia/index.mjs
@@ -1,28 +1,32 @@
 /**
  * fetchMedia
  *
- * @param {string} mediaId  The unique numerical identifier assigned to each image attachment in the media library.
+ * @param {string} mediaId The unique numerical identifier assigned to each image attachment in the media library.
  *
  * @return {Object} todo.
  */
 
 // External dependencies.
-import { useSelect } from "@wordpress/data"
-import { store as coreStore } from "@wordpress/core-data"
+import { useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
 
 export function fetchMedia(mediaId) {
-  return useSelect(
-    select => {
-      const { getMedia, isResolving, hasFinishedResolution } = select(coreStore)
+	return useSelect(
+		(select) => {
+			const { getMedia, isResolving, hasFinishedResolution } =
+				select(coreStore);
 
-      const mediaParameters = [mediaId, { context: "view" }]
+			const mediaParameters = [mediaId, { context: 'view' }];
 
-      return {
-        mediaObj: getMedia(...mediaParameters),
-        isResolvingMedia: isResolving("getMedia", mediaParameters),
-        hasResolvedMedia: hasFinishedResolution("getMedia", mediaParameters)
-      }
-    },
-    [mediaId]
-  )
+			return {
+				mediaObj: getMedia(...mediaParameters),
+				isResolvingMedia: isResolving('getMedia', mediaParameters),
+				hasResolvedMedia: hasFinishedResolution(
+					'getMedia',
+					mediaParameters
+				),
+			};
+		},
+		[mediaId]
+	);
 }
