8ff5903063fa518327973f0d05746291d169f7c2
acketon <acketon@gmail.com>
2025-07-15 13:36:54 -0400

    fix output of meta data about the post in the ResultItems component. Add support for ResultItem to render without data as a visual placeholder so Results component doesn’t render empty

--- components/PostChooser/editor-partials/results-item/index.mjs
+++ components/PostChooser/editor-partials/results-item/index.mjs
@@ -1,41 +1,67 @@
-import { Button } from '@wordpress/components';
+import { Button, ExternalLink } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { decodeEntities } from '@wordpress/html-entities';
+import { dateI18n } from '@wordpress/date';
+
+// Import CSS.
 import './editor.scss';
 
 export const ResultsItem = ( props ) => {
-	const { post, onSelectPost } = props;
+	const {
+		post,
+		onSelectPost,
+		placeholder = false,
+	} = props;
+
 
 	return (
-		<li className="bu-components-post-chooser-results-item" key={ post.id }>
-			<div className="bu-components-post-chooser-results-item-container">
-				<div className="bu-components-post-chooser-results-item-inner">
-					<div className="bu-components-post-chooser-results-item-postdetails">
-						<div className="bu-components-post-chooser-results-item-title">
-							{ post.title.rendered }
+		<>
+			<li className="bu-components-post-chooser-results-item" data-placeholder={ placeholder } key={ post?.id }>
+				<div className="bu-components-post-chooser-results-item-container">
+					<div className="bu-components-post-chooser-results-item-inner">
+						<div className="bu-components-post-chooser-results-item-postdetails">
+							<div className="bu-components-post-chooser-results-item-title">
+								{ post?.title && (
+									decodeEntities( post.title?.rendered )
+								)}
+								{post?.link &&  (
+									<span className='bu-components-post-chooser-results-item-link'>
+										<ExternalLink href={post.link}>View Post</ExternalLink>
+									</span>
+								)}
+							</div>
+							<div className="bu-components-post-chooser-results-item-metadata">
+								{ post?.modified && (
+									<span className="bu-components-post-chooser-results-item-modified">
+										<>
+											{ __( 'Modified: ' ) }
+											{ dateI18n('F j, Y, g:i a', post.modified ) }
+										</>
+									</span>
+								)}
+								{ post?.status && (
+									<span className="bu-components-post-chooser-results-item-status">
+										{ __( 'Status: ' ) }
+										{ post.status }
+									</span>
+								)}
+							</div>
 						</div>
-						<div className="bu-components-post-chooser-results-item-metadata">
-							<span className="bu-components-post-chooser-results-item-modified">
-								{ new Date(
-									post.modified
-								).toLocaleDateString() }
-							</span>
-							<span className="bu-components-post-chooser-results-item-status">
-								{ post.status }
+						<div className="bu-components-post-chooser-results-item-posttype">
+							<span className="bu-components-post-chooser-results-item-type">
+								{ post?.type }
 							</span>
 						</div>
 					</div>
-					<div className="bu-components-post-chooser-results-item-posttype">
-						<span className="bu-components-post-chooser-results-item-type">
-							{ post.type }
-						</span>
-					</div>
+					<Button
+						className="bu-components-post-chooser-item-select-button"
+						onClick={ () => onSelectPost( post ) }
+						disabled={ placeholder }
+					>
+						Select
+					</Button>
 				</div>
-				<Button
-					className="bu-components-post-chooser-item-select-button"
-					onClick={ () => onSelectPost( post ) }
-				>
-					Select
-				</Button>
-			</div>
-		</li>
+			</li>
+		</>
 	);
 };
