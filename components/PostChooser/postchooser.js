10f75cdbf98667b61094c2cda546faade69a89d4
acketon <acketon@gmail.com>
2025-07-24 14:58:57 -0400

    Remove useState for the isModalOpen. Instead the block using this component should handle that. Add an onClose method

--- components/PostChooser/postchooser.js
+++ components/PostChooser/postchooser.js
@@ -7,8 +7,6 @@
 
 // WordPress dependencies
 import { __ } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
-import { useState, useEffect } from '@wordpress/element';
 
 // Internal dependencies
 import { PostChooserModal } from './editor-partials/modal/index.js';
@@ -21,42 +19,21 @@ export const PostChooser = ( props ) => {
 		onSelectPost,
 		modalLabel,
 		modalTitle,
-		buttonLabel = __( 'Select Post' ),
 		postTypes = [ 'posts', 'pages' ], // Default post types to search.
 		searchPlaceholder,
 		minCharacters = 3,
+		onClose = () => {},
 	} = props;
 
-	/**
-	 * Modal State Handlers.
-	 *
-	 * Manages the open/closed state of the Modal
-	 * that contains the Post Chooser UI.
-	 */
-	const [ isModalOpen, setIsModalOpen ] = useState( false );
-
 	return (
-		<>
-			<Button
-				isPrimary
-				className="bu-components-post-chooser-button"
-				onClick={ () => {
-					setIsModalOpen( true );
-				} }
-			>
-				{ buttonLabel }
-			</Button>
-			{ isModalOpen && (
-				<PostChooserModal
-					onSelectPost={ onSelectPost }
-					label={ modalLabel }
-					title={ modalTitle }
-					postTypes={ postTypes }
-					placeholder={ searchPlaceholder }
-					minCharacters={ minCharacters }
-					onClose={ () => setIsModalOpen( false ) }
-				/>
-			) }
-		</>
+		<PostChooserModal
+			onSelectPost={ onSelectPost }
+			label={ modalLabel }
+			title={ modalTitle }
+			postTypes={ postTypes }
+			placeholder={ searchPlaceholder }
+			minCharacters={ minCharacters }
+			onClose={ onClose }
+		/>
 	);
 };
