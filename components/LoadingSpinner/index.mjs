f33d132f00ddcc2e482b606502d3cd864b08b246
Tim King <112511000+timkingbu@users.noreply.github.com>
2025-10-17 12:08:33 -0400

    building works (#35)

--- components/LoadingSpinner/index.mjs
+++ components/LoadingSpinner/index.mjs
@@ -16,32 +16,28 @@ import './editor.scss';
 /**
  * Returns the class list for the component based on the current settings.
  *
- * @param {string} className	Additional classes assigned to the component.
- * @param {string} text			If the component has loading text set.
- * @param {string} shadow		If the component has a shadow set.
+ * @param {string} className Additional classes assigned to the component.
+ * @param {string} text      If the component has loading text set.
+ * @param {string} shadow    If the component has a shadow set.
  */
-const getClasses = ( className, text, shadow ) => classnames(
-	'bu-components-loading-spinner',
-	{
+const getClasses = ( className, text, shadow ) =>
+	classnames( 'bu-components-loading-spinner', {
 		[ `bu-components-loading-spinner--has-shadow` ]: shadow,
 		[ `bu-components-loading-spinner--has-text` ]: text,
 		[ className ]: className,
-	}
-);
+	} );
 
 export const LoadingSpinner = ( props ) => {
-	const {
-		text = undefined,
-		shadow = true,
-		className = undefined
-	} = props;
+	const { text = undefined, shadow = true, className = undefined } = props;
 
 	return (
 		<div className={ getClasses( className, text, shadow ) }>
 			{ text && (
-				<strong className="bu-components-loading-spinner--label">{ text }</strong>
-			)}
-				<Spinner />
+				<strong className="bu-components-loading-spinner--label">
+					{ text }
+				</strong>
+			) }
+			<Spinner />
 		</div>
-	)
+	);
 };
